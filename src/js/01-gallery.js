0dd20f6bdaf372ac44b278c36aa2b9a1319b9031
Pavelmd <pashanovosvit2@gmail.com>
2023-09-05 16:27:44 +0300

    1 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
+function createGalleryItemsMarkup(galleryPhotos) {
+  return galleryPhotos
+    .map(
+      ({ preview, original, description }) =>
+        ` <li class="gallery__link">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li> `
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
 console.log(galleryItems);
+
+
